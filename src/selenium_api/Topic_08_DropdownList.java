69a952bcefbaf36f8d81dc997b9209e77d0b3cb9
trung.buinguyenthanh <trung.buinguyenthanh@DADWKS0259.dad.gameloft.org>
2020-02-10 16:57:21 +0700

    Update Topic07 & Add Topic08

---
+++ src/selenium_api/Topic_08_DropdownList.java
@@ -0,0 +1,225 @@
+package selenium_api;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_08_DropdownList {
+	WebDriver driver;
+	String fName = "Selenium";
+	String lName = "Online";
+	String email = "Selenium" + randomNumber() + "@gmail.com";
+	String pass = "123456";
+	String confirmPass = "123456";
+	 
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	 
+	@Test
+	public void TC_01_HTML_DropdownList() throws InterruptedException {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		Select selectJob1 = new Select(driver.findElement(By.id("job1")));
+		Assert.assertFalse(selectJob1.isMultiple());
+		selectJob1.selectByVisibleText("Mobile Testing");
+		Assert.assertEquals("Mobile Testing", selectJob1.getFirstSelectedOption().getText());
+		selectJob1.selectByValue("manual");
+		Assert.assertEquals("Manual Testing", selectJob1.getFirstSelectedOption().getText());
+		selectJob1.selectByIndex(9);
+		Assert.assertEquals("Functional UI Testing", selectJob1.getFirstSelectedOption().getText());
+		Assert.assertEquals(10, selectJob1.getOptions().size());
+		
+		Select selectJob2 = new Select(driver.findElement(By.id("job2")));
+		Assert.assertTrue(selectJob2.isMultiple());
+		selectJob2.selectByVisibleText("Automation");
+		selectJob2.selectByVisibleText("Mobile");
+		selectJob2.selectByVisibleText("Desktop");
+		List<WebElement> selectedList = selectJob2.getAllSelectedOptions();
+		for(WebElement list:selectedList) {
+			System.out.println(list.getText());
+		}
+		Assert.assertEquals(3, selectJob2.getAllSelectedOptions().size());
+		selectJob2.deselectAll();
+		Assert.assertEquals(0, selectJob2.getAllSelectedOptions().size());
+		
+	}
+	 
+	@Test
+	public void TC_02_HTML_DropdownList2() {
+		driver.get("https://demo.nopcommerce.com");
+		driver.findElement(By.className("ico-register")).click();
+		Assert.assertEquals("Register", driver.findElement(By.className("page-title")).getText());
+		
+		driver.findElement(By.id("gender-male")).click();
+		driver.findElement(By.id("FirstName")).sendKeys(fName);
+		driver.findElement(By.id("LastName")).sendKeys(lName);
+		driver.findElement(By.id("Email")).sendKeys(email);
+		driver.findElement(By.id("Password")).sendKeys(pass);
+		driver.findElement(By.id("ConfirmPassword")).sendKeys(confirmPass);
+		
+		Select selectDay = new Select(driver.findElement(By.name("DateOfBirthDay")));
+		selectDay.selectByVisibleText("1");
+		Assert.assertEquals(32, selectDay.getOptions().size());
+		Select selectMonth = new Select(driver.findElement(By.name("DateOfBirthMonth")));
+		selectMonth.selectByVisibleText("May");
+		Assert.assertEquals(13, selectMonth.getOptions().size());
+		Select selectYear = new Select(driver.findElement(By.name("DateOfBirthYear")));
+		selectYear.selectByVisibleText("1980");
+		Assert.assertEquals(112, selectYear.getOptions().size());
+		
+		driver.findElement(By.id("register-button")).click();
+		
+		Assert.assertEquals("Your registration completed", driver.findElement(By.className("result")).getText());
+		driver.findElement(By.name("register-continue")).click();
+		Assert.assertEquals("https://demo.nopcommerce.com/", driver.getCurrentUrl());
+		driver.findElement(By.className("ico-account")).click();
+		Assert.assertEquals("My account - Customer info", driver.findElement(By.className("page-title")).getText());
+		driver.findElement(By.className("ico-logout")).click();
+		Assert.assertEquals("https://demo.nopcommerce.com/", driver.getCurrentUrl());
+	}
+	
+	@Test
+	public void TC_03_Custom_DropdownList() throws Exception {
+		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
+		driver.findElement(By.id("number-button")).click();
+		Thread.sleep(1000);
+		List<WebElement> allitems = driver.findElements(By.xpath("//ul[@id='number-menu']/li"));
+		//System.out.println("So luong phan tu la: " +allitems.size());
+		for (WebElement childElement : allitems) {
+			//System.out.println(childElement.getText());
+			if (childElement.getText().contains("19")) {
+				if (childElement.isDisplayed()) {
+					childElement.click();
+				} else {
+					JavascriptExecutor js = (JavascriptExecutor) driver;
+					js.executeScript("arguments[0].scrollIntoView(true);", childElement);
+					Thread.sleep(1000);
+					js.executeScript("arguments[0].click();", childElement);
+				}
+				Thread.sleep(1000);
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void TC_04_Custom_DropdownList() throws Exception {
+		driver.get("https://ej2.syncfusion.com/angular/demos/?_ga=2.262049992.437420821.1575083417-524628264.1575083417#/material/drop-down-list/data-binding");
+		driver.findElement(By.id("games")).click();
+		Thread.sleep(1000);
+		List<WebElement> allitems = driver.findElements(By.xpath("//ul[@id='games_options']/li"));
+		//System.out.println("So luong phan tu la: " +allitems.size());
+		for (WebElement childElement : allitems) {
+			//System.out.println(childElement.getText());
+			if (childElement.getText().matches("Football")) {
+				if (childElement.isDisplayed()) {
+					childElement.click();
+				} else {
+					JavascriptExecutor js = (JavascriptExecutor) driver;
+					js.executeScript("arguments[0].scrollIntoView(true);", childElement);
+					Thread.sleep(1000);
+					js.executeScript("arguments[0].click();", childElement);
+				}
+				Thread.sleep(1000);
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void TC_05_Custom_DropdownList() throws Exception {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+		driver.findElement(By.xpath("//div[@role='listbox']")).click();
+		Thread.sleep(1000);
+		List<WebElement> allitems = driver.findElements(By.xpath("//div[@role='option']/span"));
+		//System.out.println("So luong phan tu la: " +allitems.size());
+		for (WebElement childElement : allitems) {
+			//System.out.println(childElement.getText());
+			if (childElement.getText().matches("Christian")) {
+				if (childElement.isDisplayed()) {
+					childElement.click();
+				} else {
+					JavascriptExecutor js = (JavascriptExecutor) driver;
+					js.executeScript("arguments[0].scrollIntoView(true);", childElement);
+					Thread.sleep(1000);
+					js.executeScript("arguments[0].click();", childElement);
+				}
+				Thread.sleep(1000);
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void TC_06_Custom_DropdownList() throws Exception {
+		driver.get("https://mikerodham.github.io/vue-dropdowns/");
+		driver.findElement(By.xpath("//li[@class='dropdown-toggle']")).click();
+		Thread.sleep(1000);
+		List<WebElement> allitems = driver.findElements(By.xpath("//ul[@class='dropdown-menu']/li/a"));
+		System.out.println("So luong phan tu la: " +allitems.size());
+		for (WebElement childElement : allitems) {
+			System.out.println(childElement.getText());
+			if (childElement.getText().matches("First Option")) {
+				if (childElement.isDisplayed()) {
+					childElement.click();
+				} else {
+					JavascriptExecutor js = (JavascriptExecutor) driver;
+					js.executeScript("arguments[0].scrollIntoView(true);", childElement);
+					Thread.sleep(1000);
+					js.executeScript("arguments[0].click();", childElement);
+				}
+				Thread.sleep(1000);
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void TC_07_Custom_DropdownList() throws Exception {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-search-selection/");
+		driver.findElement(By.xpath("//div[@role='alert']")).click();
+		Thread.sleep(1000);
+		List<WebElement> allitems = driver.findElements(By.xpath("//div[@class='item']/span"));
+		//System.out.println("So luong phan tu la: " +allitems.size());
+		for (WebElement childElement : allitems) {
+			//System.out.println(childElement.getText());
+			if (childElement.getText().matches("Bahrain")) {
+				if (childElement.isDisplayed()) {
+					childElement.click();
+				} else {
+					JavascriptExecutor js = (JavascriptExecutor) driver;
+					js.executeScript("arguments[0].scrollIntoView(true);", childElement);
+					Thread.sleep(1000);
+					js.executeScript("arguments[0].click();", childElement);
+				}
+				Thread.sleep(1000);
+				break;
+			}
+		}
+	}
+	
+	 
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	 
+	public int randomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(100000);
+	}
+}
